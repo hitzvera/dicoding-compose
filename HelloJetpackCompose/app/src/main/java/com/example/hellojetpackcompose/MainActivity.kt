3c067c609a657080412e9af0e60fc91ae8744f4d
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2022-11-23 11:41:33 +0700

    hello jetpack compose done

--- HelloJetpackCompose/app/src/main/java/com/example/hellojetpackcompose/MainActivity.kt
+++ HelloJetpackCompose/app/src/main/java/com/example/hellojetpackcompose/MainActivity.kt
@@ -1,16 +1,46 @@
 package com.example.hellojetpackcompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Devices
 import com.example.hellojetpackcompose.ui.theme.HelloJetpackComposeTheme
+
+private val sampleName = listOf(
+    "Andre",
+    "Desta",
+    "Parto",
+    "Wendy",
+    "Komeng",
+    "Raffi Ahmad",
+    "Andhika Pratama",
+    "Vincent Ryan Rompies"
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +52,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    HelloJetpackComposeApp()
                 }
             }
         }
@@ -30,14 +60,95 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun HelloJetpackComposeApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ){
+        GreetingList(names = sampleName)
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+
+    var isExpanded by remember { mutableStateOf(false) }
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow,
+        )
+    )
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "Logo jetpack compose",
+                modifier = Modifier.size(animatedSizeDp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "Hello $name!",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+
+                    )
+                Text(
+                    text = "welcome to dicoding",
+                    style = MaterialTheme.typography.body1.copy(
+                        fontStyle = FontStyle.Italic,
+                    )
+                )
+            }
+            IconButton(onClick = { isExpanded = !isExpanded}){
+                Icon(
+                    imageVector = if(isExpanded) Icons.Outlined.ExpandLess else Icons.Outlined.ExpandMore,
+                    contentDescription = if(isExpanded) "Expand less" else "Expend more",
+                )
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun GreetingList(names: List<String>) {
+    if(names.isNotEmpty()){
+        LazyColumn() {
+          items(names){ name ->
+              Greeting(name)
+          }
+        }
+    } else {
+        Box(contentAlignment = Alignment.Center){
+            Text(text = "No conent provided")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HelloJetpackComposeTheme {
-        Greeting("Android")
+        Greeting("Mujahid")
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Preview(showBackground = true, device = Devices.PIXEL_4, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun HelloJetpackAppPreview() {
+    HelloJetpackComposeTheme {
+        HelloJetpackComposeApp()
     }
 }
